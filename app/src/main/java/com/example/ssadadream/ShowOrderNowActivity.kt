72b1e6e6785b7b19ae1438f78bdacff6db10b7fc
dlwldnjs950 <dlwldnjs950@naver.com>
2019-11-03 20:50:24 +0900

    JW - 주문등록, 주문현황

--- app/src/main/java/com/example/ssadadream/ShowOrderNowActivity.kt
+++ app/src/main/java/com/example/ssadadream/ShowOrderNowActivity.kt
@@ -1,8 +1,10 @@
 package com.example.ssadadream
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_showordernow.*
+import kotlinx.android.synthetic.main.fragment_others_tab.*
 
 class ShowOrderNowActivity : AppCompatActivity() {
 
--- app/src/main/java/com/example/ssadadream/ShowOrderNow_Others_tab.kt
+++ app/src/main/java/com/example/ssadadream/ShowOrderNow_Others_tab.kt
@@ -1,11 +1,15 @@
 package com.example.ssadadream
 
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import kotlinx.android.synthetic.main.fragment_others_tab.*
+import kotlinx.android.synthetic.main.fragment_others_tab.view.*
 
 /**
  * A simple [Fragment] subclass.
@@ -16,9 +20,18 @@ class ShowOrderNow_Others_tab : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_others_tab, container, false)
-    }
+//
+//        return inflater.inflate(R.layout.fragment_others_tab, container, false)
+        val view: View = inflater!!.inflate(R.layout.fragment_others_tab, container, false)
 
+        view.btn_ordernow_others.setOnClickListener { view ->
+            Toast.makeText(context, "모든 제품을 수령하였습니다!", Toast.LENGTH_LONG).show()
+            val intent = Intent (context, MainActivity::class.java)
+            startActivity(intent)
+        }
+
+        // Return the fragment view/layout
+        return view
+    }
 
 }
